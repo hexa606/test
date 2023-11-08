7cfa9f5b839a34f15fc79b6784fba65309cc9ccb
hexa606 <148295061+hexa606@users.noreply.github.com>
2023-11-08 12:08:37 +0900

    Update taskkill.py

--- taskkill.py
+++ taskkill.py
@@ -2,7 +2,7 @@ import subprocess
 
 subprocess.call("TASKKILL /f /T /IM  msedge.exe")
 subprocess.call("TASKKILL /f /T /IM  msedgedriver.exe")
-# subprocess.call("TASKKILL /f /T  /IM conda.exe") #終わらない
+# subprocess.call("TASKKILL /f /T  /IM conda.exe") #終わらdない
 # subprocess.call("TASKKILL /f /T /IM  cmd.exe")
 subprocess.call("TASKKILL /f /T /IM  conhost.exe")
 
